0bbac2c1a0f96e665f05841abe6f95474f053fcd
claudinemahoro <claudinemah3@gmail.com>
2020-11-07 22:53:07 +0200

    goal project

---
+++ src/app/goal-service/goal.service.ts
@@ -0,0 +1,14 @@
+// import { Injectable } from '@angular/core';
+// import { Goals } from '../goals';
+// // import { GoalService } from '../goal-service/goal.service';
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class GoalService {
+
+//   getGoals(){
+//     return Goals
+//   }
+
+//   constructor() { }
+// }
